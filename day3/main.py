e1d2785a049c2f34c82f3df4aebfc88be5d9e6ab
Jesse Alloy <jalloy@stripe.com>
2022-12-03 00:44:32 -0500

    day 3

--- day3/main.py
+++ day3/main.py
@@ -1,14 +1,37 @@
 from timeit import default_timer as timer
+import string
 
 lines = [line.strip() for line in open("input.txt")]
 
+def sum_up(vals):
+    sumv = 0
+    for val in vals:
+        if val.isupper():
+            sumv += ord(val.lower()) + 26 - 96
+        else:
+            sumv += ord(val) - 96
+    return sumv
+
 def part_1():
     start = timer()
-    return None, timer() - start
+    saved = []
+    for line in lines:
+        part1 = [char for char in line[0:int(len(line)/2)]]
+        part2 = [char for char in line[int(len(line)/2):]]
+        saved.append(list(set(part1).intersection(set(part2)))[0])
+    return sum_up(saved), timer() - start
 
 def part_2():
     start = timer()
-    return None, timer() - start
+    saved = []
+    ind = 0
+    while ind < len(lines):
+        one = set([char for char in lines[ind]])
+        two = set([char for char in lines[ind + 1]])
+        three = set([char for char in lines[ind + 2]])
+        saved.append(list(one.intersection(two, three))[0])
+        ind += 3
+    return sum_up(list(saved)), timer() - start
 
 if __name__ == "__main__":
     part_1, time_1 = part_1()
